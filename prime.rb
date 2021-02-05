6fb49ed2d07f201c6666306206523c75427ec7b6
Annie Wiegel <annewiegel17@gmail.com>
2021-02-05 00:51:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,4 @@
-# Add  code here!
-def prime?(integer)
 
-  if integer > 0 && integer !% 2
-    return true
-  else
-    return false
-  end
-end
 
 def prime?(integer)
   if integer > 0 && integer !% 2
